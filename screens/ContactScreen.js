128d65f0597feeb97a54edaaa2d2e2fe5439aad5
shengrong <shengrong_88@hotmail.com>
2020-12-06 04:45:29 +0800

    investment beginner app

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,10 +1,59 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import {
+  Container,
+  Content,
+  Header,
+  List,
+  ListItem,
+  Left,
+  Right,
+  Thumbnail,
+  Body,
+  Text,
+} from "native-base";
+
+const contactsData = [
+  {
+    name: "Charlie McCharles",
+    title: "CEO",
+    company: "Baskets International LLC",
+    pic: "https://randomuser.me/portraits/men/1.jpg",
+  },
+  {
+    name: "Desiree Dee",
+    title: "CMO",
+    company: "Busket Inc",
+    pic: "https://randomuser.me/portraits/women/1.jpg",
+  },
+  {
+    name: "Adam ellis",
+    title: "CTO",
+    company: "Baskets of Biskits",
+    pic: "https://randomuser.me/portraits/men/2.jpg",
+  },
+];
 
 export default function ContactScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Contact!</Text>
-        </View>
-    );
+  return (
+    <Container>
+      <Content>
+        <List>
+          {contactsData.map((item) => (
+            <ListItem avatar key={item.name}>
+              <Left>
+                <Thumbnail source={{ uri: item.pic }} />
+              </Left>
+              <Body>
+                <Text>{item.name}</Text>
+                <Text note>
+                  {item.title}, {item.company}
+                </Text>
+              </Body>
+            </ListItem>
+          ))}
+        </List>
+      </Content>
+    </Container>
+  );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,79 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Card, CardItem, Body, Accordion } from "native-base";
 
-function HomeMainScreen() {
-    return (
-        <View>
-            <Text>Home!</Text>
-        </View>
-    );
+const dataArray = [
+    {
+      title: "What is this?",
+      content: "Exactly what the title says. A basket throwing competition!",
+    },
+    {
+      title: "Who is this by?",
+      content:
+        "The International Society of Basket Throwers (ISBT). We love throwing baskets.",
+    },
+    {
+      title: "Why is this?",
+      content: "Because baskets! Wheee!",
+    },
+   ];
+
+function HomeHomeScreen() {
+ return (
+   <View style={styles.container}>
+     <Text style={styles.titleText}>Basket Throwing Tournament 2020</Text>
+     <Card>
+       <CardItem>
+         <Body>
+           <Text style={styles.cardTitle}>Welcome</Text>
+           <Text>
+             Thanks for downloading our app! Here, you'll find all kinds of
+             information about our upcoming event. It'll be great!
+           </Text>
+         </Body>
+       </CardItem>
+       <CardItem cardBody>
+         <Image
+           source={{
+             uri:
+               "https://www.containerstore.com/catalogimages/339352/10074096-small-seagrass-belly-basket.jpg",
+           }}
+           style={{ height: 200, width: null, flex: 1 }}
+         />
+       </CardItem>
+     </Card>
+     <Accordion
+             dataArray={dataArray}
+             expanded={0}
+           />
+   </View>
+ );
 }
 
 const Stack = createStackNavigator();
 
 export default function HomeScreen() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeMainScreen} />
-        </Stack.Navigator>
-    );
+ return (
+   <Stack.Navigator>
+     <Stack.Screen name="Welcome" component={HomeHomeScreen} />
+   </Stack.Navigator>
+ );
 }
+
+const styles = StyleSheet.create({
+ container: {
+   padding: 10,
+ },
+ titleText: {
+   fontSize: 36,
+   fontWeight: "bold",
+   textAlign: "center",
+   marginBottom: 12,
+ },
+ cardTitle: {
+   fontSize: 24,
+   fontWeight: "bold",
+   marginBottom: 12,
+ },
+});
